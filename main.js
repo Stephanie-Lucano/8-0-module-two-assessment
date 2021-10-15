8ea82e0eda1687e68b9f6287d174715cbffee731
stephanie lucano <stephaniemarielucano@gmail.com>
2021-10-15 13:09:26 -0400

    Removed the return added before the forEach native array function and added a value atrribute to the option tag for each movie

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ fetch("https://ghibliapi.herokuapp.com/films/")
     .then((result) => {
         const selection = document.querySelector("#movie-titles");
 
-        return result.forEach((movie) => {
+        result.forEach((movie) => {
             const option = document.createElement("option");
+            option.value = movie.title;
             option.textContent = movie.title;
             selection.append(option);
         })
